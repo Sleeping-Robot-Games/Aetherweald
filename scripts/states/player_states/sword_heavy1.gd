931e02979631c5a560ca6e00b6eb1f3c1686102f
Ross <ross@eml.cc>
2023-03-29 22:01:36 -0700

    allow rolling out of heavy attacks

--- scripts/states/player_states/sword_heavy1.gd
+++ scripts/states/player_states/sword_heavy1.gd
@@ -1,14 +1,17 @@
 extends BaseState
 
 @export var idle_node: NodePath
+@export var roll_node: NodePath
 @export var run_node: NodePath
 @export var sword_heavy2_node: NodePath
 @onready var idle_state: BaseState = get_node(idle_node)
+@onready var roll_state: BaseState = get_node(roll_node)
 @onready var run_state: BaseState = get_node(run_node)
 @onready var sword_heavy2_state: BaseState = get_node(sword_heavy2_node)
 
 var is_done: bool = false
 var is_combo: bool = false
+var is_rolling: bool = false
 var combo_window: float = 0.9
 var move_window: float = 0.2
 var attack_dir: Vector2 = Vector2.ZERO
@@ -18,12 +21,15 @@ func enter() -> void:
 	super.enter()
 	is_done = false
 	is_combo = false
+	is_rolling = false
 	attack_dir = actor.direction
 
 func input(event: InputEvent) -> BaseState:
 	if event.is_action_pressed('heavy_attack') \
 	and animation_player.current_animation_position <= combo_window:
 		is_combo = true
+	elif event.is_action_pressed('roll'):
+		is_rolling = true
 	return null
 
 func connect_anim_player() -> void:
@@ -38,12 +44,14 @@ func physics_process(_delta: float) -> BaseState:
 		is_done = true
 	
 	# update direction if holding input and exiting state
-	if is_done:
+	if is_rolling or is_done:
 		input_dir = get_input_dir()
 		if input_dir != Vector2.ZERO:
 			actor.direction = input_dir
 	
-	if is_done:
+	if is_rolling:
+		return roll_state
+	elif is_done:
 		if is_combo:
 			return sword_heavy2_state
 		elif input_dir == Vector2.ZERO:
--- scripts/states/player_states/sword_heavy2.gd
+++ scripts/states/player_states/sword_heavy2.gd
@@ -1,11 +1,14 @@
 extends BaseState
 
 @export var idle_node: NodePath
+@export var roll_node: NodePath
 @export var run_node: NodePath
 @onready var idle_state: BaseState = get_node(idle_node)
+@onready var roll_state: BaseState = get_node(roll_node)
 @onready var run_state: BaseState = get_node(run_node)
 
 var is_done: bool = false
+var is_rolling: bool = false
 var move_window: Dictionary = {
 	'start': 0.4,
 	'end': 0.6
@@ -16,7 +19,13 @@ var speed: float = 300.0
 func enter() -> void:
 	super.enter()
 	is_done = false
+	is_rolling = false
 	attack_dir = actor.direction
+
+func input(event: InputEvent) -> BaseState:
+	if event.is_action_pressed('roll'):
+		is_rolling = true
+	return null
 
 func connect_anim_player() -> void:
 	animation_player.animation_finished.connect(_on_animation_finished)
@@ -28,12 +37,14 @@ func physics_process(_delta: float) -> BaseState:
 	var anim_seconds = animation_player.current_animation_position
 	
 	# update direction if holding input and exiting state
-	if is_done:
+	if is_rolling or is_done:
 		input_dir = get_input_dir()
 		if input_dir != Vector2.ZERO:
 			actor.direction = input_dir
-			
-	if is_done:
+	
+	if is_rolling:
+		return roll_state
+	elif is_done:
 		if input_dir == Vector2.ZERO:
 			return idle_state
 		else:
